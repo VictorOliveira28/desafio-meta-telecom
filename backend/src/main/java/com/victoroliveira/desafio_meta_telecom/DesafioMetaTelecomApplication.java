0ba8482709d41f05e393c4c538c3dcb2217b58c7
Victor Oliveira <victor.oliveira20@icloud.com>
2025-08-05 07:32:10 -0300

    Project started

---
+++ backend/src/main/java/com/victoroliveira/desafio_meta_telecom/DesafioMetaTelecomApplication.java
@@ -0,0 +1,13 @@
+package com.victoroliveira.desafio_meta_telecom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioMetaTelecomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioMetaTelecomApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/victoroliveira/desafio_meta_telecom/DesafioMetaTelecomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victoroliveira.desafio_meta_telecom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioMetaTelecomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
