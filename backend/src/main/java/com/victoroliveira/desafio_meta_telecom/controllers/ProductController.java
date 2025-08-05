00d6d0105bd4bbb5ed4c45148fa4669e87313531
Victor Oliveira <victor.oliveira20@icloud.com>
2025-08-05 10:18:01 -0300

    bug fixed

--- backend/src/main/java/com/victoroliveira/desafio_meta_telecom/controllers/ProductController.java
+++ backend/src/main/java/com/victoroliveira/desafio_meta_telecom/controllers/ProductController.java
@@ -1,11 +1,18 @@
 package com.victoroliveira.desafio_meta_telecom.controllers;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.victoroliveira.desafio_meta_telecom.dto.ProductDTO;
 import com.victoroliveira.desafio_meta_telecom.services.ProductService;
@@ -24,6 +31,26 @@ public class ProductController {
 	public ResponseEntity<List<ProductDTO>> findAll(){
 		List<ProductDTO> list = service.findAll();
 		return ResponseEntity.ok(list);		
+	}
+	
+	@PostMapping
+	public ResponseEntity<ProductDTO> insert( @RequestBody ProductDTO dto){
+		dto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(dto.id()).toUri();		
+		return ResponseEntity.created(uri).body(dto);
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<ProductDTO> update(@PathVariable Long id,  @RequestBody ProductDTO dto){
+		dto = service.update(id, dto);			
+		return ResponseEntity.ok().body(dto);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id){
+		service.delete(id);			
+		return ResponseEntity.noContent().build();
 	}
 
 }
